229c075be045999a05e91a7f582bfaca6e9b138e
sveltekits <sveltekit.com@gmail.com>
2024-06-15 17:02:01 +0900

    commit

--- aexample_kit/src/routes/+page.svelte
+++ aexample_kit/src/routes/+page.svelte
@@ -4,7 +4,15 @@
 
     const { page, status } = data;
 
-    console.log("main PageServerData :: ", page, status);
+    console.log("PageServerData :: ", page, status);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+{#await data}
+    <p>Loading......</p>
+{:then value}
+    <p>{JSON.stringify(data)}</p>
+    <!-- {"Data":{"Page":"+Layout.Server.Ts","Status":"Ok"},"Page":"+Page.Server.Ts","Status":"Ok"} -->
+{:catch error}
+    <p>Error loading.... {error}</p>
+{/await}
